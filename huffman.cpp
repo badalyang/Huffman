4563ad9efb920dcb377e3cae3062f955300daaaf
badalyang <126382208+badalyang@users.noreply.github.com>
2024-11-27 10:11:55 +0400

    huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -33,8 +33,7 @@ struct compare {
     }
 };
 
-void printCodes(Node* root, string str,
-                unordered_map<char, string>& huffmanCode)
+void printCodes(Node* root, string str, unordered_map<char, string>& huffmanCode)
 {
     if (root == nullptr)
         return;
@@ -47,24 +46,47 @@ void printCodes(Node* root, string str,
     printCodes(root->right, str + "1", huffmanCode);
 }
 
+// Serialize the Huffman Tree for storing
+void serializeTree(Node* root, string& serialized)
+{
+    if (root == nullptr) {
+        serialized += "#"; // Use '#' as a marker for null nodes
+        return;
+    }
+    serialized += root->ch;
+    serializeTree(root->left, serialized);
+    serializeTree(root->right, serialized);
+}
+
+// Deserialize the Huffman Tree
+Node* deserializeTree(string& serialized, int& index)
+{
+    if (index >= serialized.size() || serialized[index] == '#') {
+        index++;
+        return nullptr;
+    }
+    Node* node = new Node(serialized[index], 0);
+    index++;
+    node->left = deserializeTree(serialized, index);
+    node->right = deserializeTree(serialized, index);
+    return node;
+}
+
 void buildHuffmanTree(string text)
 {
     unordered_map<char, int> freq;
     for (char ch : text) {
         freq[ch]++;
     }
+
     priority_queue<Node*, vector<Node*>, compare> pq;
     for (auto pair : freq) {
         pq.push(new Node(pair.first, pair.second));
     }
+
     while (pq.size() != 1) {
-      
-        Node* left = pq.top();
-        pq.pop();
-        Node* right = pq.top();
-        pq.pop();
-
-
+        Node* left = pq.top(); pq.pop();
+        Node* right = pq.top(); pq.pop();
         int sum = left->freq + right->freq;
         pq.push(new Node('\0', sum, left, right));
     }
@@ -74,44 +96,48 @@ void buildHuffmanTree(string text)
     unordered_map<char, string> huffmanCode;
     printCodes(root, "", huffmanCode);
 
-   
+    // Compress: Encode the string
+    string encodedStr = "";
+    for (char ch : text) {
+        encodedStr += huffmanCode[ch];
+    }
+
+    // Serialize the Huffman Tree
+    string serializedTree = "";
+    serializeTree(root, serializedTree);
+
+    // Print the results
     cout << "Huffman Codes:\n";
     for (auto pair : huffmanCode) {
         cout << pair.first << " " << pair.second << "\n";
     }
 
-    // Print nermucac texty
     cout << "\nOriginal string:\n" << text << "\n";
+    cout << "\nEncoded string:\n" << encodedStr << "\n";
+    cout << "\nSerialized Tree:\n" << serializedTree << "\n";
 
-    // Print 
-    string str = "";
-    for (char ch : text) {
-        str += huffmanCode[ch];
-    }
-    cout << "\nEncoded string:\n" << str << "\n";
+    // Decompress: Decode the string
+    auto decode = [&](string encodedStr, string serializedTree) {
+        int index = 0;
+        Node* deserializedRoot = deserializeTree(serializedTree, index);
 
-   
-    auto decode = [&](string str) {
         cout << "\nDecoded string:\n";
-        Node* curr = root;
-        for (char bit : str) {
+        Node* curr = deserializedRoot;
+        for (char bit : encodedStr) {
             if (bit == '0') {
                 curr = curr->left;
-            }
-            else {
+            } else {
                 curr = curr->right;
             }
-
-            // terev
             if (!curr->left && !curr->right) {
                 cout << curr->ch;
-                curr = root;
+                curr = deserializedRoot;
             }
         }
         cout << "\n";
     };
 
-    decode(str);
+    decode(encodedStr, serializedTree);
 }
 
 int main()
